5c65b5c16f2aba455a5b2a22146172df34cda0b3
sundhar <sundhar.v@zohocorp.com>
2021-03-16 14:38:21 +0530

    challenge form visual changes

--- app/component/ChallengeForm/ChallengeForm.js
+++ app/component/ChallengeForm/ChallengeForm.js
@@ -19,63 +19,69 @@ export default class ChallengeForm extends Component {
       onTagChange
     } = this.props;
     return (
-      <div className='hero hero-sm bg-gray form-container'>
-        <div className='hero-body'>
-          <h1>{isEditForm ? 'Edit Challenge' : 'Add Challenge'}</h1>
-          <form>
-            <fieldset disabled={isFormDisabled}>
-              <div className='form-group'>
-                <label className='form-label label-lg' htmlFor='challenge-title'>
-                  Title
-                </label>
-                <input
-                  className='form-input input-lg input-element'
-                  type='text'
-                  id='challenge-title'
-                  placeholder='Title'
-                  maxLength={TITLE_MAX_LENGTH}
-                  value={title}
-                  onChange={onTitleChange}
-                />
+      <div className='container grid-lg'>
+        <div className='columns'>
+          <div className='column col-xs-6'>
+            <div className='hero hero-sm bg-gray form-container'>
+              <div className='hero-body'>
+                <h1>{isEditForm ? 'Edit Challenge' : 'Add Challenge'}</h1>
+                <form>
+                  <fieldset disabled={isFormDisabled}>
+                    <div className='form-group'>
+                      <label className='form-label label-lg' htmlFor='challenge-title'>
+                        Title
+                      </label>
+                      <input
+                        className='form-input input-lg input-element'
+                        type='text'
+                        id='challenge-title'
+                        placeholder='Title'
+                        maxLength={TITLE_MAX_LENGTH}
+                        value={title}
+                        onChange={onTitleChange}
+                      />
+                    </div>
+                    <div className='form-group'>
+                      <label className='form-label label-lg' htmlFor='challenge-description'>
+                        Description
+                      </label>
+                      <textarea
+                        className='form-input input-lg'
+                        id='challenge-description'
+                        placeholder='Description'
+                        rows='15'
+                        maxLength={DESCRIPTION_MAX_LENGTH}
+                        value={description}
+                        onChange={onDescriptionChange}
+                      ></textarea>
+                    </div>
+                    <div className='form-group'>
+                      <label className='form-label label-lg' htmlFor='challenge-tag'>
+                        Tag
+                      </label>
+                      <input
+                        className='form-input input-lg'
+                        type='text'
+                        id='challenge-tag'
+                        placeholder='Enter the tags as comma seperated values'
+                        maxLength={TAG_MAX_LENGTH}
+                        value={tag}
+                        onChange={onTagChange}
+                      />
+                    </div>
+                    <div className='form-action-button'>
+                      <button className='btn btn-primary' onClick={null}>
+                        Submit
+                      </button>
+                      <button className='btn btn-link' onClick={null}>
+                        Clear
+                      </button>
+                    </div>
+                  </fieldset>
+                </form>
               </div>
-              <div className='form-group'>
-                <label className='form-label label-lg' htmlFor='challenge-description'>
-                  Description
-                </label>
-                <textarea
-                  className='form-input input-lg'
-                  id='challenge-description'
-                  placeholder='Description'
-                  rows='15'
-                  maxLength={DESCRIPTION_MAX_LENGTH}
-                  value={description}
-                  onChange={onDescriptionChange}
-                ></textarea>
-              </div>
-              <div className='form-group'>
-                <label className='form-label label-lg' htmlFor='challenge-tag'>
-                  Tag
-                </label>
-                <input
-                  className='form-input input-lg'
-                  type='text'
-                  id='challenge-tag'
-                  placeholder='Enter the tags as comma seperated values'
-                  maxLength={TAG_MAX_LENGTH}
-                  value={tag}
-                  onChange={onTagChange}
-                />
-              </div>
-              <div className='form-action-button'>
-                <button className='btn btn-primary' onClick={null}>
-                  Submit
-                </button>
-                <button className='btn btn-link' onClick={null}>
-                  Clear
-                </button>
-              </div>
-            </fieldset>
-          </form>
+            </div>
+          </div>
         </div>
       </div>
     );
